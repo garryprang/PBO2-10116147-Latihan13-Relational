102ffaf81cc0f37779c416fee666670f949cee66
grryv <grryv@grry>
2020-10-21 19:52:33 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan13/relational/PBO210116147Latihan13Relational.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10116147.latihan13.relational;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk mengimplementasikan operator relasional.
+ */
+
+public class PBO210116147Latihan13Relational {
+
+   
+    public static void main(String[] args) {
+        
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2 ));
+        System.out.println("a != b = " + (bil1 != bil2 ));
+        System.out.println("a > b = " + (bil1 > bil2 ));
+        System.out.println("a < b = " + (bil1 < bil2 ));
+        System.out.println("a >= b = " + (bil2 >= bil1 ));
+        System.out.println("a <= b = " + (bil2 <= bil1 ));
+        
+             
+   
+    }
+    
+}
